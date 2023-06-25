818c1eaabe8e96b281435a9e3c9a873b3c39260c
Kalahan Santhiago De Oliveira <ksodesign@gmail.com>
2023-06-25 19:45:29 -0300

    Add Product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './products/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produtos', component: ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { ProductComponent } from './products/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
